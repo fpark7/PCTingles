4d34a92a322382b42a9ad7c12ac8628392877528
Jon Ting <jting416@gmail.com>
2017-03-01 16:13:46 -0500

    Added error checking from the server - only errors if email is used again.  Otherwise, succeeds.  Also limited state input to only 2 chars.

--- registerUser.php
+++ registerUser.php
@@ -1,14 +1,20 @@
+<!DOCTYPE html>
 <?php
 $db = new mysqli('localhost', 'root', '', 'pctingles');
 if ($db->connect_error):
 	die ("Could not connect to db: " . $db->connect_error);
 endif;
+$pass = True;
 $split[0] = $_POST["name"];
 $split[1] = $_POST["email"];
 $split[2] = $_POST["password"];
 $split[3] = $_POST["school"];
 $split[4] = $_POST["year"];
-$split[5] = $_POST["newsletter"];
+if(isset($_POST["newsletter"])) {
+	$split[5] = $_POST["newsletter"];
+} else {
+	$split[5] = "off";
+}
 $split[6] = $_POST["address"];
 $split[7] = $_POST["city"];
 $split[8] = $_POST["state"];
@@ -35,12 +41,95 @@ $query = "INSERT INTO Login (email, password, name, school, year, newsletter, ad
 $result = $db->query($query);
 
 if(!$result) {
-	die("Query failed: " . $db->error);
+	$error = "Looks like your email already exists!  Please use a different one.";
+	$pass = False;
 }
 
-//Have a condition 
-header("Location: confirm.html");
-
-print_r ($split); 
-
+if ($pass) {
+	header("Location: confirm.html");
+}
 ?>
+
+<html>
+	<head>
+		<script>
+		function goBack() {
+			window.history.back()
+		}
+		</script>
+		<title>Confirmation | PC Tingles</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html">P.C.Tingles</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="index.html">Home</a></li>
+											<li><a href="about.html">About Us</a></li>
+											<li><a href="signup.html">Sign Up</a></li>
+											<li><a href="#">Log In</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<article id="main">
+						<section class="wrapper style5">
+							<div class="inner">
+								<h2>Sorry!</h2>
+								<p><?PHP echo $error ?></p>
+
+								<hr />
+
+								<ul class = "actions">
+									<li><button onclick="goBack()" class="button special">Go back</a></li>
+								</ul>
+							</div>
+						</section>
+					</article>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+							<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>&copy; P.C.Tingles</li><!--<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>-->
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
